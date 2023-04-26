2d470ddd8c33ca3339f58d97610c369328ca8c64
yoobeommo <105355963+yoobeommo@users.noreply.github.com>
2023-04-26 10:06:05 +0900

    Add files via upload

---
+++ week2/src/main/java/com/example/week2/dto/BlogRequestDto.java
@@ -0,0 +1,12 @@
+package com.example.week2.dto;
+
+import lombok.Getter;
+
+@Getter
+public class BlogRequestDto {
+    private String title;
+    private String contents;
+    private String username;
+    private String password;
+
+}
---
+++ week2/src/main/java/com/example/week2/dto/SignupRequestDto.java
@@ -0,0 +1,13 @@
+package com.example.week2.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class SignupRequestDto {
+    private String username;
+    private String password;
+    private boolean admin = false;
+    private String adminToken = "";
+}
